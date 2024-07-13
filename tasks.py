1b87069d5c71557dffbc50583db01e4ac1f0e886
Victoroide <cvictorhugo39@gmail.com>
2024-07-13 14:25:12 -0400

    fix: Update target_time calculation for monitoring exchange rate in tasks.py

--- tasks.py
+++ tasks.py
@@ -5,7 +5,7 @@ from firebase_client import get_latest_usdt_to_bob, get_historical_usdt_to_bob
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import io
-import discord, commands
+import discord
 from bot_setup import get_bot, get_subscribed_channels
 
 bot = get_bot()
@@ -14,15 +14,18 @@ default_timezone = 'America/La_Paz'
 
 @tasks.loop(hours=24)
 async def monitor_exchange_rate():
+    print("Ejecutando tarea programada para monitorear el tipo de cambio.")
     tz = pytz.timezone(default_timezone)
     now = datetime.now(tz)
-    target_time = now.replace(hour=14, minute=18, second=0, microsecond=0)  
+    target_time = now.replace(hour=14, minute=26, second=30, microsecond=0)  
     if now > target_time:
         target_time = target_time + timedelta(days=1)
     await discord.utils.sleep_until(target_time)
 
+    print("Obteniendo precio actual...")
     price = get_latest_usdt_to_bob()
     if price:
+        print(f"Precio obtenido: {price:.3f} BOB")
         historical_data = get_historical_usdt_to_bob(temporality='daily')
         timestamps = []
         prices = []
@@ -34,11 +37,9 @@ async def monitor_exchange_rate():
         fig, ax = plt.subplots()
         ax.plot(timestamps, prices, marker='o')
         ax.set(xlabel='Fecha', ylabel='Precio (BOB)', title='Historial por Día')
-        
         ax.xaxis.set_major_locator(mdates.DayLocator(interval=1))
         ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-        fig.autofmt_xdate()
-
+        fig.autofmt_xdate() 
         ax.grid()
 
         buf = io.BytesIO()
@@ -51,13 +52,16 @@ async def monitor_exchange_rate():
         for guild_id, channel_id in subscribed_channels.items():
             channel = bot.get_channel(channel_id)
             if channel:
+                print(f"Enviando mensaje a canal {channel.name} (ID: {channel_id}) en servidor {channel.guild.name} (ID: {guild_id})")
                 await channel.send(
                     f"El precio actual de USDT a BOB es: {price:.3f} BOB",
                     file=file
                 )
+        buf.close() 
     else:
         print('No se pudo obtener el precio actual.')
 
 @monitor_exchange_rate.before_loop
 async def before_monitor_exchange_rate():
     await bot.wait_until_ready()
+    print("Bot listo para monitorear el tipo de cambio.")
