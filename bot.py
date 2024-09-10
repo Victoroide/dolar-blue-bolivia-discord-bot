074c5711d02fc6383b04c5c35bafbde364b157e2
Victoroide <cvictorhugo39@gmail.com>
2024-09-09 20:46:59 -0400

    feat: Add command synchronization and error handling in on_ready function

--- bot.py
+++ bot.py
@@ -7,6 +7,13 @@ bot = get_bot()
 @bot.event
 async def on_ready():
     print(f'Bot conectado como {bot.user}')
+    
+    try:
+        synced = await bot.tree.sync() 
+        print(f"Se han sincronizado {len(synced)} comandos de aplicación (slash commands).")
+    except Exception as e:
+        print(f"Error al sincronizar los comandos de aplicación: {e}")
+    
     monitor_exchange_rate.start()
 
 if __name__ == "__main__":
